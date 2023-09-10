ae73f9799fba7a73409805840f09adec1b329597
Ethan Borg <eborg4@uwo.ca>
2023-09-10 18:30:32 -0400

    Created an acces point from port 5000

--- server/index.js
+++ server/index.js
@@ -1,2 +1,13 @@
+//Constants
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Server
+app.listen(5000, () => {
+  console.log("server has started on port 5000");
+});
