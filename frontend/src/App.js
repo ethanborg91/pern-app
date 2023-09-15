c5dbb17fb7b4081ecbc432ea121f19081f515ecc
Ethan Borg <eborg4@uwo.ca>
2023-09-14 20:24:48 -0400

    Created the frontend folder with an initialized react framework

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import "./App.css";
+import InputTodo from "./components/Input";
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Input.js
@@ -0,0 +1,36 @@
+import React, { Fragment, useState } from "react";
+
+const Input = () => {
+  const [description, setDescription] = useState("");
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: { "Content-T": "application/json" },
+        body: JSON.stringify(body),
+      });
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="text-center mt-5">Todo List</h1>
+      <form className="d-flex mt-5">
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="btn btn-success">Submit</button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default Input;
