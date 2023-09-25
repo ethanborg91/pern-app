1e907809534630f5ab5e83bb78ec6745022342d0
Ethan Borg <eborg4@uwo.ca>
2023-09-25 15:49:10 -0400

    Added the list of items in a table as well as the delete button for each object

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import InputTodo from "./components/Input";
+import ListTodo from "./components/List";
 
 function App() {
   return (
     <Fragment>
       <div className="container">
         <InputTodo />
+        <ListTodo />
       </div>
     </Fragment>
   );
--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -7,11 +7,13 @@ const Input = () => {
     e.preventDefault();
     try {
       const body = { description };
-      const response = fetch("http://localhost:5000/todos", {
+      const response = await fetch("http://localhost:5000/todos", {
         method: "POST",
-        headers: { "Content-T": "application/json" },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
       });
+
+      window.location = "/";
     } catch (err) {
       console.error(err.message);
     }
@@ -20,7 +22,7 @@ const Input = () => {
   return (
     <Fragment>
       <h1 className="text-center mt-5">Todo List</h1>
-      <form className="d-flex mt-5">
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
         <input
           type="text"
           className="form-control"
